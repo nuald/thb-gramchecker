89a53cf2fb88602a84db53257ae1457370a2a2e1
Alexander Slesarev <alex.slesarev@gmail.com>
2010-10-20 16:32:04 +1100

    Scope refactoring has been made.

--- grammarchecker/content/overlay.js
+++ grammarchecker/content/overlay.js
@@ -34,250 +34,245 @@
  * 
  * ***** END LICENSE BLOCK ***** */
 
-const overlayCss = "chrome://grammarchecker/skin/overlay.css";
+if ("undefined" == typeof(grammarchecker)) {
+    const Cc = Components.classes;
+    const Ci = Components.interfaces;
+    var grammarchecker = {
+        onLoad: function() {
+            this._overlayCss = "chrome://grammarchecker/skin/overlay.css";
+            this._strings = document.getElementById("grammarchecker-strings");
+            this._nodesMapping = new NodesMapping();
+            this._ranges = [];
 
-var grammarchecker = {
-	onLoad: function() {
-		// initialization code
-		this.initialized = true;
-		this.strings = document.getElementById("grammarchecker-strings");
-		this.nodesMapping = new NodesMapping();
-		this.ranges = [];
+            let that = this;
+            let nsGrammarCommand = {
+                isCommandEnabled: function(aCommand, dummy) {
+                    return (IsDocumentEditable() &&	!IsInHTMLSourceMode());
+                },
+                getCommandStateParams: function(aCommand, aParams, aRefCon) {},
+                doCommandParams: function(aCommand, aParams, aRefCon) {},
+                doCommand: function(aCommand) {
+                    that.onMenuItemCommand(aCommand);
+                }
+            };
+            let commandTable = GetComposerCommandTable();
+            commandTable.registerCommand("cmd_grammar", nsGrammarCommand);
+        },
+        clearPreview: function() {
+            let preview = document.getElementById("grammarchecker-preview");
+            while (preview.firstChild) {
+                //The list is LIVE so it will re-index each call
+                preview.removeChild(preview.firstChild);
+            };
+            this._ranges = [];
+            let editor = GetCurrentEditor();
+            if (editor != null) {
+                this._prepareEditor(editor);
+            }
+        },
+        _prepareEditor: function(editor) {
+            editor.QueryInterface(nsIEditorStyleSheets);
+            editor.addOverrideStyleSheet(this._overlayCss);
+            let xmlDoc = editor.document;
+            let nsResolver = xmlDoc.createNSResolver(
+                                                     xmlDoc.ownerDocument == null ? xmlDoc.documentElement : xmlDoc.ownerDocument.documentElement);
 
-		var nsGrammarCommand = {
-			isCommandEnabled: function(aCommand, dummy) {
-				return (IsDocumentEditable() &&	!IsInHTMLSourceMode());
-			},
-			getCommandStateParams: function(aCommand, aParams, aRefCon) {},
-			doCommandParams: function(aCommand, aParams, aRefCon) {},
-			doCommand: function(aCommand) {
-				grammarchecker.onMenuItemCommand(aCommand);
-			}
-		};
-		var commandTable = GetComposerCommandTable();
-		commandTable.registerCommand("cmd_grammar", nsGrammarCommand);
-	},
-	clearPreview: function() {
-		var preview=document.getElementById("grammarchecker-preview");
-		while (preview.firstChild) {
-			//The list is LIVE so it will re-index each call
-			preview.removeChild(preview.firstChild);
-		};
-		this.ranges = [];
-		var editor = GetCurrentEditor();
-		if (editor != null) {
-			this.prepareEditor(editor);
-		}
-	},
-	prepareEditor: function(editor) {
-		editor.QueryInterface(nsIEditorStyleSheets);
-		editor.addOverrideStyleSheet(overlayCss);
-		var xmlDoc = editor.document;
-		var nsResolver = xmlDoc.createNSResolver(
-			xmlDoc.ownerDocument == null ?
-			    xmlDoc.documentElement : xmlDoc.ownerDocument.documentElement);
+            let getPath = function() {
+                return xmlDoc.evaluate("//span[@class='grammarchecker-highlight']", xmlDoc, nsResolver, XPathResult.ANY_UNORDERED_NODE_TYPE, null).singleNodeValue;
+            };
+            let node = getPath();
+            while (node != null) {
+                let s = this._prepareSelection();
+                let range = xmlDoc.createRange();
+                range.selectNode(node);
+                s.addRange(range);
+                editor.removeInlineProperty("span", "class");
+                node = getPath();
+            }
+        },
+        _isInRanges: function(fromx, tox, y) {
+            let index = fromx + "," + tox + "," + y;
+            if (this._ranges.indexOf(index) == -1) {
+                this._ranges.push(index);
+                return false;
+            }
+            return true;
+        },
+        _prepareSelection: function() {
+            let editor = GetCurrentEditor();
+            let s = editor.selection;
+            if (s.rangeCount > 0) {
+                s.removeAllRanges();
+            }
+            return s;
+        },
+        _createDescription: function(item, li) {
+            this._nodesMapping.init();
+            let editor = GetCurrentEditor();
+            let s = editor.selection;
 
-		var getPath = function() {
-			return xmlDoc.evaluate("//span[@class='grammarchecker-highlight']",
-			   xmlDoc, nsResolver, XPathResult.ANY_UNORDERED_NODE_TYPE,
-			   null).singleNodeValue;
-		};
-		var node = getPath();
-		while (node != null) {
-			var s = this.prepareSelection();
-			var range = xmlDoc.createRange();
-			range.selectNode(node);
-			s.addRange(range);
-			editor.removeInlineProperty("span", "class");
-			node = getPath();
-		}
-	},
-	isInRanges: function(fromx, tox, y) {
-		var index = fromx + "," + tox + "," + y;
-		if (this.ranges.indexOf(index) == -1) {
-			this.ranges.push(index);
-			return false;
-		}
-		return true;
-	},
-	prepareSelection: function() {
-		var editor = GetCurrentEditor();
-		var s = editor.selection;
-		if (s.rangeCount > 0) {
-			s.removeAllRanges();
-		}
-		return s;
-	},
-	createDescription: function(item, li) {
-		this.nodesMapping.init();
-		var editor = GetCurrentEditor();
-		var s = editor.selection;
+            let context = item.attributes["context"].textContent;
+            let offset = parseInt(item.attributes["contextoffset"].textContent);
+            let len = parseInt(item.attributes["errorlength"].textContent);
 
-		var context = item.attributes["context"].textContent;
-		var offset = parseInt(item.attributes["contextoffset"].textContent);
-		var len = parseInt(item.attributes["errorlength"].textContent);
+            let fromx = parseInt(item.attributes["fromx"].textContent);
+            let fromy = parseInt(item.attributes["fromy"].textContent);
+            let tox = parseInt(item.attributes["tox"].textContent) - 1;
 
-		var fromx = parseInt(item.attributes["fromx"].textContent);
-		var fromy = parseInt(item.attributes["fromy"].textContent);
-		var tox = parseInt(item.attributes["tox"].textContent) - 1;
-
-		if (!this.isInRanges(fromx, tox, fromy)) {
-			var startItem = this.nodesMapping.findNode(fromx, fromy);
-			var endItem = this.nodesMapping.findNode(tox, fromy);
+            if (!this._isInRanges(fromx, tox, fromy)) {
+                let startItem = this._nodesMapping.findNode(fromx, fromy);
+                let endItem = this._nodesMapping.findNode(tox, fromy);
 		
-			if (startItem != null && endItem != null) {
-				var range = document.createRange();
-				range.setStart(startItem.node, startItem.offset);
-				range.setEnd(endItem.node, endItem.offset);
-				var s = this.prepareSelection();
-				s.addRange(range);
-				var atomService = Components.classes["@mozilla.org/atom-service;1"].getService(Components.interfaces.nsIAtomService);
-				var span = atomService.getAtom("span");
-				editor.QueryInterface(nsIHTMLEditor);
-				editor.setInlineProperty(span, "class",
-										 "grammarchecker-highlight");
-			}
-		}
+                if (startItem != null && endItem != null) {
+                    let range = document.createRange();
+                    range.setStart(startItem.node, startItem.offset);
+                    range.setEnd(endItem.node, endItem.offset);
+                    let s = this._prepareSelection();
+                    s.addRange(range);
+                    let atomService = Cc["@mozilla.org/atom-service;1"].getService(Ci.nsIAtomService);
+                    let span = atomService.getAtom("span");
+                    editor.QueryInterface(nsIHTMLEditor);
+                    editor.setInlineProperty(span, "class",
+                                             "grammarchecker-highlight");
+                }
+            }
 
-		var contextLen = context.length;
-		var overall = offset + len;
+            let contextLen = context.length;
+            let overall = offset + len;
 
-		var left = String(context).substring(0, offset);
-		var middle = String(context).substring(offset, overall);
-		var right = String(context).substring(overall, contextLen);
-		if (left.length > 0) {
-			li.appendChild(document.createTextNode(left));
-		}
-		if (middle.length > 0) {
-			var bold = document.createElement("b");
-			bold.appendChild(document.createTextNode(middle));
-			li.appendChild(bold);
-		}
-		if (right.length > 0) {
-			li.appendChild(document.createTextNode(right));
-		}
-	},
-	addRule: function(item, ul) {
-		var msg = item.attributes["msg"].textContent;
-		var replacements = item.attributes["replacements"].textContent;
-		var append = function(labelId, text) {
-			var li = document.createElement("li");
-			var label = grammarchecker.strings.getString(labelId);
-			li.appendChild(document.createTextNode(label + ": " + text));
-		    ul.appendChild(li);
-		};
-		append("ruleMessage", msg);
-		append("replacementsMessage", replacements);
-    },
-    showNodes: function(nodes) {
-		this.clearPreview();
-		var preview=document.getElementById("grammarchecker-preview");
-		var ul = document.createElement("ul");
-		preview.appendChild(ul);
-		for (var i = 0; i < nodes.snapshotLength; i++) {
-			var item = nodes.snapshotItem(i);
-			var li = document.createElement("li");
-			this.createDescription(item, li);
-			ul.appendChild(li);
-			var innerUl = document.createElement("ul");
-			this.addRule(item, innerUl);
-			ul.appendChild(innerUl);
-		}
-		this.prepareSelection();
-	},
-	showResult: function(xmlDoc) {
-		var nsResolver = xmlDoc.createNSResolver(
-			xmlDoc.ownerDocument == null ?
-			    xmlDoc.documentElement : xmlDoc.ownerDocument.documentElement);
+            let left = String(context).substring(0, offset);
+            let middle = String(context).substring(offset, overall);
+            let right = String(context).substring(overall, contextLen);
+            if (left.length > 0) {
+                li.appendChild(document.createTextNode(left));
+            }
+            if (middle.length > 0) {
+                let bold = document.createElement("b");
+                bold.appendChild(document.createTextNode(middle));
+                li.appendChild(bold);
+            }
+            if (right.length > 0) {
+                li.appendChild(document.createTextNode(right));
+            }
+        },
+        _addRule: function(item, ul) {
+            let that = this;
+            let msg = item.attributes["msg"].textContent;
+            let replacements = item.attributes["replacements"].textContent;
+            let append = function(labelId, text) {
+                let li = document.createElement("li");
+                let label = that._strings.getString(labelId);
+                li.appendChild(document.createTextNode(label + ": " + text));
+                ul.appendChild(li);
+            };
+            append("ruleMessage", msg);
+            append("replacementsMessage", replacements);
+        },
+        _showNodes: function(nodes) {
+            this.clearPreview();
+            let preview=document.getElementById("grammarchecker-preview");
+            let ul = document.createElement("ul");
+            preview.appendChild(ul);
+            for (let i = 0; i < nodes.snapshotLength; i++) {
+                let item = nodes.snapshotItem(i);
+                let li = document.createElement("li");
+                this._createDescription(item, li);
+                ul.appendChild(li);
+                let innerUl = document.createElement("ul");
+                this._addRule(item, innerUl);
+                ul.appendChild(innerUl);
+            }
+            this._prepareSelection();
+        },
+        _showResult: function(xmlDoc) {
+            let nsResolver = xmlDoc.createNSResolver(
+                                                     xmlDoc.ownerDocument == null ? xmlDoc.documentElement : xmlDoc.ownerDocument.documentElement);
 
-		var nodes = xmlDoc.evaluate('/matches/error', xmlDoc, nsResolver,
-			XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null );
+            let nodes = xmlDoc.evaluate('/matches/error', xmlDoc, nsResolver,
+                                        XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null );
 
-		if (nodes.snapshotLength > 0) {
-			this.showNodes(nodes);
-		} else {
-			this.showText("noErrorsMessage", null);
-		}
-	},
-	showText: function(textId, appendix) {
-		this.clearPreview();
-		var preview = document.getElementById("grammarchecker-preview");
-		var text = this.strings.getString(textId);
-		if (appendix != null) {
-			text += appendix;
-		}
-		var textNode = document.createTextNode(text);
-		preview.appendChild(textNode);
-	},
-	showError: function(errorId) {
-		this.clearPreview();
-		var preview = document.getElementById("grammarchecker-preview");
-		var text = this.strings.getString(errorId);
-		var errorMsg = document.createTextNode(text);
-		var errorDiv = document.createElement("div");
-		errorDiv.setAttribute("id", "grammarchecker-error");
-		errorDiv.appendChild(errorMsg);
-		preview.appendChild(errorDiv);
-	},
-	getLangFromSpellChecker: function(prefs) {
-		var editor = GetCurrentEditor();
-		try {
-			var gSpellChecker = editor.getInlineSpellChecker(true);
-			var lang = gSpellChecker.spellChecker.GetCurrentDictionary();
-			return lang.substr(0, 2);
-		} catch (ex) {
-			return prefs.getCharPref("langpref");
-		}
+            if (nodes.snapshotLength > 0) {
+                this._showNodes(nodes);
+            } else {
+                this._showText("noErrorsMessage", null);
+            }
+        },
+        _showText: function(textId, appendix) {
+            this.clearPreview();
+            let preview = document.getElementById("grammarchecker-preview");
+            let text = this._strings.getString(textId);
+            if (appendix != null) {
+                text += appendix;
+            }
+            let textNode = document.createTextNode(text);
+            preview.appendChild(textNode);
+        },
+        _showError: function(errorId) {
+            this.clearPreview();
+            let preview = document.getElementById("grammarchecker-preview");
+            let text = this._strings.getString(errorId);
+            let errorMsg = document.createTextNode(text);
+            let errorDiv = document.createElement("div");
+            errorDiv.setAttribute("id", "grammarchecker-error");
+            errorDiv.appendChild(errorMsg);
+            preview.appendChild(errorDiv);
+        },
+        _getLangFromSpellChecker: function(prefs) {
+            let editor = GetCurrentEditor();
+            try {
+                let gSpellChecker = editor.getInlineSpellChecker(true);
+                let lang = gSpellChecker.spellChecker.GetCurrentDictionary();
+                return lang.substr(0, 2);
+            } catch (ex) {
+                return prefs.getCharPref("langpref");
+            }
+        },
+        _checkGrammar: function(server, lang, errorHandler) {
+            let that = this;
+            this._showText("processingMessage"," "+server+" ["+lang+"] ...");
 
-	},
-	checkGrammar: function(server, lang, errorHandler) {
-		this.showText("processingMessage"," "+server+" ["+lang+"] ...");
-
-		//Get the html Source message document
-		var htmlSource = encodeURIComponent(this.nodesMapping.init());
-		var xhr=Components.classes["@mozilla.org/xmlextras/xmlhttprequest;1"];
-		var req = xhr.createInstance(Components.interfaces.nsIXMLHttpRequest);
-		req.open('POST', server, true);
-		req.setRequestHeader('Content-Type',
-							 'application/x-www-form-urlencoded');
-		req.onreadystatechange = function(){
-			if (req.readyState == 4) {
-				var result = req.responseXML;
-				if (result == null) {
-					grammarchecker.showError("errorMessage");
-					errorHandler();
-				} else {
-					grammarchecker.showResult(result);
-				}
-			}
-		};
-		try {
-			req.send("language=" + lang + "&text=" + htmlSource);
-		} catch (ex) {
-			this.showError("errorMessage");
-			errorHandler();
-		}
-	},
-	onMenuItemCommand: function(e) {
-		var prefs = Components.classes["@mozilla.org/preferences-service;1"]
-		    .getService(Components.interfaces.nsIPrefService)
+            //Get the html Source message document
+            let htmlSource = encodeURIComponent(this._nodesMapping.init());
+            let xhr = Cc["@mozilla.org/xmlextras/xmlhttprequest;1"];
+            let req = xhr.createInstance(Ci.nsIXMLHttpRequest);
+            req.open('POST', server, true);
+            req.setRequestHeader('Content-Type',
+                                 'application/x-www-form-urlencoded');
+            req.onreadystatechange = function(){
+                if (req.readyState == 4) {
+                    let result = req.responseXML;
+                    if (result == null) {
+                        that._showError("errorMessage");
+                        errorHandler();
+                    } else {
+                        that._showResult(result);
+                    }
+                }
+            };
+            try {
+                req.send("language=" + lang + "&text=" + htmlSource);
+            } catch (ex) {
+                this._showError("errorMessage");
+                errorHandler();
+            }
+        },
+        onMenuItemCommand: function(e) {
+            let that = this;
+            let prefs = Cc["@mozilla.org/preferences-service;1"]
+		    .getService(Ci.nsIPrefService)
 		    .getBranch("extensions.grammarchecker.");
-		prefs.QueryInterface(Components.interfaces.nsIPrefBranch2);
-		var server1 = prefs.getCharPref("urlpref1");
-		var server2 = prefs.getCharPref("urlpref2");
-		var lang = this.getLangFromSpellChecker(prefs);
-		this.checkGrammar(server1, lang, function() {
-				grammarchecker.checkGrammar(server2, lang, null);
-			});
-	}
+            prefs.QueryInterface(Ci.nsIPrefBranch2);
+            let server1 = prefs.getCharPref("urlpref1");
+            let server2 = prefs.getCharPref("urlpref2");
+            let lang = this._getLangFromSpellChecker(prefs);
+            this._checkGrammar(server1, lang, function() {
+                    that._checkGrammar(server2, lang, null);
+                });
+        }
+    };
 };
-window.addEventListener(
-	"load",
-    function(e) { grammarchecker.onLoad(e); },
-    false
-);
-window.addEventListener(
-    "compose-window-init",
-    function() { grammarchecker.clearPreview(); },
-    true
-);
+window.addEventListener("load",function(e) { grammarchecker.onLoad(e); },
+                        false);
+window.addEventListener("compose-window-init",
+                        function() { grammarchecker.clearPreview(); },
+                        true);
