c5aa7e1078969edbe00646bd08507723e718dcc3
Nuald <Nuald@Nuald-PC>
2011-07-22 20:46:53 -0700

    Mothertongue support has been added.

--- grammarchecker/defaults/preferences/grammarchecker.js
+++ grammarchecker/defaults/preferences/grammarchecker.js
@@ -1,3 +1,4 @@
 pref("extensions.grammarchecker.urlpref1", "http://localhost:8081");
 pref("extensions.grammarchecker.urlpref2", "http://archeus.ro/languagetool/check");
 pref("extensions.grammarchecker.langpref", "en");
+pref("extensions.grammarchecker.mothertongue", "");
