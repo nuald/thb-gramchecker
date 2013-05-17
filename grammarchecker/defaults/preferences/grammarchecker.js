42f6c0255e63dd839e8494f877502e2ad05af583
Alexander Slesarev <alex.slesarev@gmail.com>
2013-05-17 15:21:05 -0700

    Default language server has been updated.

--- grammarchecker/defaults/preferences/grammarchecker.js
+++ grammarchecker/defaults/preferences/grammarchecker.js
@@ -1,4 +1,4 @@
 pref("extensions.grammarchecker.urlpref1", "http://localhost:8081");
-pref("extensions.grammarchecker.urlpref2", "http://archeus.ro/languagetool/check");
+pref("extensions.grammarchecker.urlpref2", "https://languagetool.org:8081/");
 pref("extensions.grammarchecker.langpref", "en");
 pref("extensions.grammarchecker.mothertongue", "");
